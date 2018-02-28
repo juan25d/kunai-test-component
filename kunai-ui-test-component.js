c7afad983c6e800b0d1ef84c2da40f076e2eb5f3
Juan Antonio Sanchez Arbos <juan.antonio@sunagile.com>
2018-02-28 14:43:13 +0100

    Test component

---
+++ kunai-ui-test-component.js
@@ -0,0 +1,34 @@
+
+const root = '/app/'
+const componentName = 'testComponent';
+const fileName = 'kunai-ui-test-component';
+
+angular.module(componentName, [])
+    .component(componentName, {
+        templateUrl: `${root}${componentName}/${fileName}.html`,
+        bindings: {
+            literalParemeter: '@?',           
+            bidirectionalParemeter : '=?',
+            onewayParemeter : '<?'
+        },
+        controller: function () {
+            let ctrl = this;
+			console.info("controller")
+            ctrl.$onInit = function () {
+				console.log('Initial onewayParemeter');
+				console.info(ctrl.onewayParemeter);
+				console.log('Initial bidirectionalParemeter');
+				console.info(ctrl.bidirectionalParemeter)
+
+            };
+			ctrl.validate = function(){
+				
+				console.log('validate called')				
+				ctrl.onewayParemeter = angular.isString(ctrl.bidirectionalParemeter) && ctrl.bidirectionalParemeter.length > 0
+				console.log(ctrl.onewayParemeter )				
+
+			}
+        }
+    });
+
+export default componentName;
